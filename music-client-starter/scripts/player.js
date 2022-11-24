d07929c26eca418544d99dba398c7764fac4d5a9
saioufe <saioufeyoutube@yahoo.com>
2022-11-24 12:11:57 -0600

    map

--- music-client-starter/scripts/player.js
+++ music-client-starter/scripts/player.js
@@ -146,7 +146,6 @@ function loadDataPlayLists(songs) {
                         <button class="pladd" id="removetoplaylist" data-id = "'+value.id+ '" data-number="' + value.songId + '">-</button> \
                     </div> \
                 </li>');
-
                 }),
                 trackCount = songs.length,
                 npAction = $('#npAction'),
